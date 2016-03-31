8f21bad3701c3db06466dd2aaad2e8525eca117e
Stephen Johnson <stephenjjohnson@gmail.com>
2016-03-31 09:07:59 -0500

    Added units column in codebook().
    Added alignment of columns to codebook.table()

--- codebook.R
+++ codebook.R
@@ -1,7 +1,7 @@
 # function to make a table of names, class and  description of each column of a dataframe.
 # suitable for feeding to pander() to help in assembling a codebook
 # Author: Stephen Johnson
-# Date: 2016-03-07
+# Date: 2016-03-31
 #
 
 
@@ -10,7 +10,7 @@
 codebook <- function(df) {
         if (!(is.data.frame(df)))
                 stop("not a dataframe")
-        data.frame(Class = sapply(df, class), Description = "")
+        data.frame(Class = sapply(df, class), Units = "", Description = "")
 }
 
 
@@ -19,6 +19,8 @@ codebook.table <- function(df) {
         # df.out <- codebook(df)
         require(pander)
         panderOptions("table.style", "rmarkdown")
-        panderOptions("table.alignment.default", "left")
+        # panderOptions("table.alignment.default", "left")
+        # panderOptions("table.alignment.rownames", "left")
+        set.alignment(default = c('centre', 'left', 'left'), row.names = "left")
         pander(codebook(df))
 }
